9914b3dd4ea80c6e3015440a9371d197ea0a01de
jimmyjaxon <jimmyjaxon695@gmail.com>
2026-04-13 12:23:54 +0300

    Add combineUsers function

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+function combineUsers(...arrays) {
+  const users = [].concat(...arrays);
+  const today = new Date();
+  const merge_date = today.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  });
+  return { users, merge_date };
+}
 
-
-
-module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
-};
+module.exports = { combineUsers };
